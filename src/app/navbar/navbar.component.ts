f7f152d312a20a6240118efe16f3582785343e47
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-06-04 15:57:19 +0530

    route

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MenuService } from '../services/menu.service';
 import { MenuItem } from '../interfaces/interfaces.module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,9 +10,13 @@ import { MenuItem } from '../interfaces/interfaces.module';
 })
 export class NavbarComponent {
   menuItems: MenuItem[] = [];
-  constructor(private menuService: MenuService) {}
+  constructor(private menuService: MenuService, private router: Router) {}
 
   ngOnInit() {
     this.menuItems = this.menuService.getMenuItems();
+  }
+
+  navigateToRoute(route: string) {
+    this.router.navigateByUrl(route);
   }
 }
